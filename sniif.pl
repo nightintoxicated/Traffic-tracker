b25fae1c491b226f117660074cb7b55d413ee343
nightintoxicated <50459012+nightintoxicated@users.noreply.github.com>
2023-11-04 19:14:31 +0000

    Create sniif.pl

---
+++ sniif.pl
@@ -0,0 +1,31 @@
+use strict;
+use warnings;
+use Net::Pcap::Easy;
+
+# all arguments to new are optoinal
+my $npe = Net::Pcap::Easy->new(
+    dev              => "enp0s3",
+    filter           => "not port 22",
+    packets_per_loop => 10,
+    bytes_to_capture => 1024,
+    promiscuous      => 0, # true or false
+
+    tcp_callback => sub {
+        my ($npe, $ether, $ip, $tcp, $header ) = @_;
+        my $xmit = localtime( $header->{tv_sec} );
+
+        print "$xmit TCP: $ip->{src_ip}:$tcp->{src_port}"
+         . " -> $ip->{dest_ip}:$tcp->{dest_port}\n";
+
+         #print "\t$ether->{src_mac} -> $ether->{dest_mac}\n" if $SHOW_MAC;
+    },
+
+    icmp_callback => sub {
+        my ($npe, $ether, $ip, $icmp, $header ) = @_;
+        my $xmit = localtime( $header->{tv_sec} );
+
+        print "$xmit ICMP: $ether->{src_mac}:$ip->{src_ip}"
+         . " -> $ether->{dest_mac}:$ip->{dest_ip}\n";
+    },
+);
+

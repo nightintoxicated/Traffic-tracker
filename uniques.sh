bb5607e9f52b80164280b1ce76c44ba2cd884b29
nightintoxicated <50459012+nightintoxicated@users.noreply.github.com>
2024-09-05 12:12:22 +0100

    Update uniques.sh

--- uniques.sh
+++ uniques.sh
@@ -1,4 +1,16 @@
+#!/bin/bash
+
 if [ -e capture ] ; then
-cat capture | sort | uniq | sed 's/[A-Z].*[0-9][0-9]:[0-9][0-9]:[0-9][0-9] 2[0-9][0-9][0-9] //g'
-else echo "no capture file in same directory";
+
+  if [ -n "$1" ]; then
+    src_ip=$1;
+    echo "searching for ip $src_ip";
+    cat capture | sort | uniq | sed 's/[A-Z].*[0-9][0-9]:[0-9][0-9]:[0-9][0-9] 2[0-9][0-9][0-9] //g' | grep $src_ip;
+  else
+    cat capture | sort | uniq | sed 's/[A-Z].*[0-9][0-9]:[0-9][0-9]:[0-9][0-9] 2[0-9][0-9][0-9] //g';
+  fi
+
+else
+echo "no capture file in same directory";
 fi
+unset src_ip;
